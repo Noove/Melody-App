859e924efb07b7211c5271f5ce2b96318449fa26
Samuel Šoltys <samuelsasanka@gmail.com>
2024-04-06 17:04:16 +0200

    Added shadcn and top bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import PianoRoll from "./components/PianoRoll";
 import Footer from "./components/Footer";
-
+import TopBar from "./components/TopBar";
 function App() {
   return (
     <>
       <div className="flex h-screen w-full flex-col bg-black">
         {/* TOP CONTENT */}
+        <TopBar />
+
+        {/* MAIN CONTENT */}
         <div className="flex-1">
           <aside>{/* SIDEBAR */}</aside>
 
