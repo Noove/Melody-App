76ce9e4c376b9db136c7b2bcc8b3798293893b41
Samuel Šoltys <samuelsasanka@gmail.com>
2024-04-06 16:33:18 +0200

    Added space key to play/stop

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Volume, Volume1, Volume2, Play, Square, Settings } from "lucide-react";
-
 import CircleButton from "./CircleButton";
 
 const Footer = () => {
   const [volume, setVolume] = useState(50);
   const [isPlaying, setIsPlaying] = useState(false);
+
+  console.log(isPlaying);
+  useEffect(() => {
+    const handleKeyPress = (e: KeyboardEvent) => {
+      if (e.code === "Space") {
+        setIsPlaying((prevState) => !prevState);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyPress);
+    return () => document.removeEventListener("keydown", handleKeyPress);
+  }, []);
 
   return (
     <>
@@ -29,7 +40,7 @@ const Footer = () => {
           />
         </div>
 
-        <span className="mr-10 text-xl text-white">00:00.00</span>
+        <span className="mr-10 text-2xl text-white">00:00.00</span>
 
         <CircleButton
           icon={isPlaying ? <Square color="#fff" /> : <Play color="#fff" />}
@@ -41,7 +52,7 @@ const Footer = () => {
           icon={<Settings color="#fff" />}
         />
 
-        <button className="text-l bg-highlight flex h-12 items-center justify-center rounded-md px-4 text-white">
+        <button className="text-l flex h-12 items-center justify-center rounded-md bg-highlight px-4 text-white">
           {(window as any).chrome ? "Send to device" : "Save to file"}
         </button>
       </footer>
