d1c934e938919da80f09b963c6c715c8fd90b587
Viktor Demčák <viktorik170@gmail.com>
2024-04-06 13:18:43 +0200

    Some small progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold'>Hello world!</h1>
+      <div className="flex h-screen w-full flex-col bg-black">
+        {/* TOP CONTENT */}
+        <div className="flex-1">
+          <aside>{/* SIDEBAR */}</aside>
+
+          <div>{/* PIANO ROLL */}</div>
+        </div>
+
+        {/* FOOTER */}
+        <footer className="w-full bg-white/5 px-10">
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value="50"
+            className="slider"
+            id="myRange"
+          />
+        </footer>
+      </div>
     </>
   );
 }
