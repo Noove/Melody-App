ac0acc7fa2701364fcddaadba9265115084e70b9
Viktor Demčák <viktorik170@gmail.com>
2024-04-06 14:21:12 +0200

    Semi-functional tile placement

--- src/components/PianoRoll.tsx
+++ src/components/PianoRoll.tsx
@@ -1,43 +1,111 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
+import { create } from "zustand";
+
+type TTileStore = {
+  tiles: [number, number][];
+  addTile: (i: number, j: number) => void;
+};
+
+const tileStore = create<TTileStore>((set) => ({
+  tiles: [],
+  addTileToStore: (i: number, j: number) =>
+    set((state) => ({ tiles: [...state.tiles, [i, j]] })),
+}));
 
 const PianoRoll = () => {
   const canvasContainer = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  useEffect(() => {
+  const { tiles, addTileToStore } = tileStore();
+
+  const dpr = window.devicePixelRatio || 1;
+  const dh = 50;
+  const dw = 220;
+
+  function resizeCanvas() {
     if (!canvasRef.current || !canvasContainer.current) return;
 
-    console.log(canvasContainer.current.clientHeight, canvasContainer.current.clientWidth);
+    canvasRef.current.width = canvasContainer.current.clientWidth * dpr;
+    canvasRef.current.height = canvasContainer.current.clientHeight * dpr;
 
-    const context = canvasRef.current.getContext("2d");
+    // Redraw background
+    drawBackgroundCells();
 
-    canvasRef.current.width = canvasContainer.current.clientWidth;
-    canvasRef.current.height = canvasContainer.current.clientHeight;
+    // Redraw tiles if there are any
+    console.log(tiles);
+    if (tiles) {
+      console.log("redrawing cells");
+      tiles.forEach(([i, j]) => drawCell(i, j));
+    }
+  }
 
-    const dh = 20;
-    const dw = 100;
+  function drawBackgroundCells() {
+    if (!canvasRef.current) return;
+    const context = canvasRef.current?.getContext("2d");
+    if (!context) return;
+
     const width = canvasRef.current.width;
     const height = canvasRef.current.height;
-
-    if (context === null) return;
 
     for (let i = 0; i < height / dh; i++) {
       context.save();
 
-      context.fillStyle = i % 2 === 0 ? "#bbb" : "#aaa";
+      context.fillStyle = i % 2 === 0 ? "rgb(15,15,15)" : "rgb(20,20,20";
       context.fillRect(0, i * dh, width, i * dh + dh);
       context.restore();
     }
     for (let i = 1; i < Math.ceil(width / dw); i++) {
       context.save();
       context.strokeStyle =
-        i % 4 === 0 ? "rgba(0,0,0,.3)" : "rgba(255,255,255,.3)";
+        i % 4 === 0 ? "rgba(255,255,255,.6)" : "rgba(255,255,255,.2)";
       context.beginPath();
       context.moveTo(i * dw, 0);
       context.lineTo(i * dw + 1, height);
       context.stroke();
       context.restore();
     }
+  }
+
+  function drawCell(i: number, j: number) {
+    if (!canvasRef.current || !canvasContainer.current) return;
+    const context = canvasRef.current?.getContext("2d");
+    if (!context) return;
+
+    const cellRounding = [10, 10, 10, 10];
+
+    context.strokeStyle = "rgb(120,35,230)";
+    context.fillStyle = "rgba(120,35,230)";
+
+    context.beginPath();
+    context.roundRect(j * dw, i * dh, dw, dh, cellRounding);
+    context.stroke();
+    context.fill();
+  }
+
+  function handleCanvasClick(e: MouseEvent) {
+    if (!canvasRef.current) return;
+    const rect = canvasRef.current.getBoundingClientRect();
+    const x = (e.clientX - rect.left) * dpr;
+    const y = (e.clientY - rect.top) * dpr;
+
+    const i = Math.floor(y / dh);
+    const j = Math.floor(x / dw);
+
+    addTileToStore(i, j);
+
+    drawCell(i, j);
+  }
+
+  useEffect(() => {
+    resizeCanvas();
+
+    canvasRef.current?.addEventListener("click", handleCanvasClick);
+    window.addEventListener("resize", resizeCanvas);
+
+    return () => {
+      window.removeEventListener("resize", resizeCanvas);
+      canvasRef.current?.removeEventListener("click", handleCanvasClick);
+    };
   }, []);
 
   return (
